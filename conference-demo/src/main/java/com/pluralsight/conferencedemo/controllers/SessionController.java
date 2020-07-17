b1cb66b735bb71ae63fa24832c04057b34718ea7
unknown <ballester.alexander@gmail.com>
2020-07-18 04:14:30 +0800

    provided create, delete and update method in the controllers

--- conference-demo/src/main/java/com/pluralsight/conferencedemo/controllers/SessionController.java
+++ conference-demo/src/main/java/com/pluralsight/conferencedemo/controllers/SessionController.java
@@ -2,15 +2,10 @@ package com.pluralsight.conferencedemo.controllers;
 
 import com.pluralsight.conferencedemo.models.Session;
 import com.pluralsight.conferencedemo.repositories.SessionRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -35,5 +30,19 @@ public class SessionController {
     @ResponseStatus(HttpStatus.CREATED)
     public Session create(@RequestBody final Session session) {
         return sessionRepository.saveAndFlush(session);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable final Long id) {
+        // also need to checck for children records before deleting
+        sessionRepository.deleteById(id);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+    public Session update(@PathVariable Long id, @RequestBody Session session) {
+        Session existingSession = sessionRepository.getOne(id);
+        BeanUtils.copyProperties(session, existingSession, "session_id");
+        return sessionRepository.saveAndFlush(existingSession);
+
     }
 }
